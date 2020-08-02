a0b621cfbcf9bb75bf81c615edcaf5f429de0ef1
Brandon Gart <brandongart@gmail.com>
2020-08-02 16:21:32 -0600

    adds database populate script

---
+++ app/scripts/populateDB.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const mongodb = require("mongodb");
+const config = require("../config.js")
+
+const url = `mongodb://${config.db.host}:${config.db.port}`
+const token = "4tydCgEV45lkdBXd8-ZBKw"
+const dbName = config.db.name;
+const MongoClient = mongodb.MongoClient;
+
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+async function main() {
+	await client.connect();
+
+	const db = await client.db(dbName);
+
+	for(let key in config.populate) {
+		const collection = db.collection(key);
+		const result = await axios({
+			method: "post",
+			url: "https://app.fakejson.com/q",
+			data: { token, data: config.populate[key] }
+		});
+	
+		console.log(result.data);
+
+		const insertResult = await collection.insertMany(result.data);
+
+		console.log(insertResult);
+	}
+
+
+
+
+}
+
+main()
