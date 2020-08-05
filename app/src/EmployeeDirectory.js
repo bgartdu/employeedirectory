2ee27ca8e92206baec08995fd68cb60fe2141385
Brandon Gart <brandongart@gmail.com>
2020-08-05 12:21:30 -0600

    adds display of data from api

--- app/src/EmployeeDirectory.js
+++ app/src/EmployeeDirectory.js
@@ -2,25 +2,73 @@ import React from 'react';
 import EmployeeLine from "./EmployeeLine"
 
 class EmployeeDirectory extends React.Component {
-	render() {
-		return <div className="row">
-			<div className="col s12 center">
-				<input id = "searchField"></input>
-			</div>
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		};
+	}
+	
+	async componentDidMount() {
+		try {
+			let res = await fetch("https://randomuser.me/api/?results=100")
+			let result = await res.json();
+			let employees = result.results.map(it => {
+				return {
+					name: it.name.first + " " + it.name.last,
+					email: it.email,
+					dob: it.dob.date,
+					cell: it.cell,
+					picture: it.picture.thumbnail
+				}
+			})
 			
-			<div className="col s1">Image</div>
-			<div className="col s3">Name</div>
-			<div className="col s2">Phone</div>
-			<div className="col s4">Email</div>
-			<div className="col s2">DOB</div>
+			this.setState({
+				isLoaded: true,
+				items: employees,
+			})
+		} catch (error){
+			this.setState({
+				isLoaded: true,
+				error,
+			})
+		}
+	}
+	
+	
+	render() {
 
-			<EmployeeLine name = "bob" cell = "303-777-1832" email = "bob@google.com" dob = "5-22-1977" picture = "nothing.jpg" />
-			<EmployeeLine name = "bob" cell = "303-777-1832" email = "bob@google.com" dob = "5-22-1977" picture = "nothing.jpg" />
-			<EmployeeLine name = "bob" cell = "303-777-1832" email = "bob@google.com" dob = "5-22-1977" picture = "nothing.jpg" />
-			<EmployeeLine name = "bob" cell = "303-777-1832" email = "bob@google.com" dob = "5-22-1977" picture = "nothing.jpg" />
-			<EmployeeLine name = "bob" cell = "303-777-1832" email = "bob@google.com" dob = "5-22-1977" picture = "nothing.jpg" />
+		const { error, isLoaded, items } = this.state;
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>
+		}
+		
+		return  <div className="row">
+		<div className="col s12 center">
+		<input id = "searchField"></input>
+		</div>
+		
+		<div className="col s1">Image</div>
+		<div className="col s3">Name</div>
+		<div className="col s2">Phone</div>
+		<div className="col s4">Email</div>
+		<div className="col s2">DOB</div>
 
+		<div className="col s12 white-text"><h5>spacer</h5></div>
+		
+		{items.map(item => (
+			<EmployeeLine picture = {item.picture}
+									name={item.name}
+									cell={item.cell}
+									email={item.email}
+									dob={item.dob}/>
 
+		))}
+		
 		</div>
 	}
 }
